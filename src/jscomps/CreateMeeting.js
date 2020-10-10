5c4be1a3887c7133679a24a64a92a03d6ba12f9b
Adam Loke <adam.loke@hyperisland.se>
2020-10-10 09:59:50 +0200

    added tailwind

--- src/jscomps/CreateMeeting.js
+++ src/jscomps/CreateMeeting.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Context } from '../context/MyProvider';
 import AgendaItem from './AgendaItem';
 import ShowList from './ShowList';
@@ -7,81 +7,88 @@ import styled from 'styled-components';
 import RoomViewComponent from '../components/RoomViewComponent';
 
 const Form = styled.div({
-    height: '30rem',
-    width: '20rem',
-    margin: '10rem auto',
-    padding: '0.5rem'
-  });
-
+  height: '30rem',
+  width: '20rem',
+  margin: '10rem auto',
+  padding: '0.5rem'
+});
 
 const CreateMeeting = props => {
+  const { state, changeState } = React.useContext(Context);
+  const [showForm, setShowForm] = useState(true);
+  const [agenda, setAgenda] = useState(false);
 
-    const {state,changeState} = React.useContext(Context);
-    const [define,setDefine] = useState(true);
-    const [agenda,setAgenda] = useState(false);
+  const handleAddAgenda = item => {
+    let newList = [...state['meetingAgenda'], item];
+    changeState('meetingAgenda', newList);
+  };
 
-    const handleAddAgenda = item =>{
-        let newList = [...state['meetingAgenda'],item];
-        changeState('meetingAgenda', newList);
-    };
-
-    const handleDeleteAgenda = text =>{
-
-        let currentAgendas = state.meetingAgenda;
-        let Delindex = -1;
-        currentAgendas.map((item,index) =>{
-            if(item.item === text){Delindex= index}
-        })
-        if(Delindex > -1) {
-            let newList = [...state.meetingAgenda];
-            newList.splice(Delindex,1);
-            changeState('meetingAgenda', newList);
-        }
+  const handleDeleteAgenda = text => {
+    let currentAgendas = state.meetingAgenda;
+    let Delindex = -1;
+    currentAgendas.map((item, index) => {
+      if (item.item === text) {
+        Delindex = index;
+      }
+    });
+    if (Delindex > -1) {
+      let newList = [...state.meetingAgenda];
+      newList.splice(Delindex, 1);
+      changeState('meetingAgenda', newList);
     }
+  };
 
-    return(
-        <Form>
-            {
-                define && 
-                <div style={{display:'flex',flexDirection:'column'}}>
-                    <label>Room Name</label>
-                    <input 
-                    type='text' 
-                    name='roomName'
-                    onChange ={e => changeState('roomName',e.target.value)}
-                    ></input>
-                    
-                    <label>Meeting Title</label>
-                    <input 
-                    type='text' 
-                    name='meetingTitle'
-                    onChange ={e => changeState('meetingTitle',e.target.value)}
-                    ></input>
-                    <br />
-                    <button style = {{width: '50%'}} className ="ui primary button" onClick ={() => setAgenda(!agenda)}> + Agenda</button>
-                    {
-                        agenda &&
-                        <>
-                            <AgendaItem handleAddAgenda = {handleAddAgenda}/>
-                        </>
-                    }
-                    <br />
-                    <div>Agenda List</div>
-                    {
-                        state.meetingAgenda && <ShowList list ={state.meetingAgenda} handleClickIcon = {handleDeleteAgenda} />
-                    }
-                    <br />
-                    <br />
-                    <button style = {{width: '50%'}} className ="ui primary button" onClick={props.handleCreateMeeting}>Create Meeting</button>
-                    <br />
-                </div>
+  return (
+    <Form>
+      {showForm && (
+        <div className="flex flex-col">
+          <label>Room Name</label>
+          <input
+            className="border border-black-4"
+            type="text"
+            name="roomName"
+            onChange={e => changeState('roomName', e.target.value)}
+          ></input>
 
-            }
-        </Form>
-        
-    );
-
-}
+          <label>Meeting Title</label>
+          <input
+            type="text"
+            name="meetingTitle"
+            onChange={e => changeState('meetingTitle', e.target.value)}
+          ></input>
+          <br />
+          <button
+            style={{ width: '50%' }}
+            className="ui primary button"
+            onClick={() => setAgenda(!agenda)}
+          >
+            {' '}
+            + Agenda
+          </button>
+          {agenda && (
+            <>
+              <AgendaItem handleAddAgenda={handleAddAgenda} />
+            </>
+          )}
+          <br />
+          <div>Agenda List</div>
+          {state.meetingAgenda && (
+            <ShowList list={state.meetingAgenda} handleClickIcon={handleDeleteAgenda} />
+          )}
+          <br />
+          <br />
+          <button
+            style={{ width: '50%' }}
+            className="ui primary button"
+            onClick={props.handleCreateMeeting}
+          >
+            Create Meeting
+          </button>
+          <br />
+        </div>
+      )}
+    </Form>
+  );
+};
 
 export default CreateMeeting;
-
--- src/jscomps/ShowList.js
+++ src/jscomps/ShowList.js
@@ -1,16 +1,17 @@
 import { Icon } from '@material-ui/core';
 import React from 'react';
-import IconTrash from './IconTrash'
+import IconTrash from './IconTrash';
 
 const ShowList = props => {
+  const renderList = props.list.map((item, index) => {
+    return (
+      <div key={index}>
+        {item.item} <IconTrash text={item.item} handleClickIcon={props.handleClickIcon} />
+      </div>
+    );
+  });
 
-    const renderList = props.list.map((item,index) => {
-        return(
-            <div key={index}>{item.item} <IconTrash text = {item.item} handleClickIcon = {props.handleClickIcon} /></div>
-        );
-    });
-
-    return(<> {props.list && renderList}</>);
-}
+  return <> {props.list && renderList}</>;
+};
 
 export default ShowList;
